da391c206e2fd0f82f784b2e1211d34d75252cce
Roald Ruiter <roaldruiter@gmail.com>
2025-02-05 22:42:06 +0100

    add nice title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'bombi rocks 🎤',
+  description: 'Bombi is rockin\'!',
 }
 
 export default function RootLayout({
